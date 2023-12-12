25b3ca3c6e69a784aa226cfda69f724198993a90
archiash <archiash.legacy@gmail.com>
2023-12-12 15:12:33 +0700

    Push All

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 using namespace std;
 
 int main()
@@ -13,5 +13,29 @@ int main()
     case 'C': cout << "You have received 2000 coins.\n";
     case 'D': cout << "You have received very KAK items.\n";
   }
+  return 0;
+}
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char rank;
+  cout << "Input your rank: ";
+  cin >> rank;
+  bool pass = false;
+
+  if(rank == 'S') pass = true;
+  if(pass) cout << "You have received Super Ultra Rare Unit!!!\n";
+  if(rank == 'A') pass = true;
+  if(pass) cout << "You have received 5 gems.\n";
+  if(rank == 'B') pass = true;
+  if(pass) cout << "You have received 1 gems.\n";
+  if(rank == 'C') pass = true;
+  if(pass) cout << "You have received 2000 coins.\n";
+  if(rank == 'D') pass = true;
+  if(pass) cout << "You have received very KAK items.\n";
   return 0;
 }
--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,52 @@
 "Brook"
 "Jinbe"
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age, height, bounty;
+    bool checkHeight = false;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age > 60){
+        cout << "Your character = Brook";
+        return 0;
+    }
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+    
+        if(height < 100){
+            cout << "Your character = Chopper";
+            return 0;
+        }
+
+        if(height < 180){
+            cout << "Your character = Usopp";
+            return 0;
+        }
+        checkHeight = true;
+    }
+    
+    cout << "Enter your bounty: ";
+    cin >> bounty;
+
+
+    if(!checkHeight && bounty > 500000000){
+        cout << "Your character = Jinbe";
+        return 0;
+    }else if(!checkHeight){
+        cout << "Your character = Franky";
+        return 0;
+    }
+
+    if(bounty > 1100000000){
+        cout << "Your character = Zoro";
+        return 0;
+    }else{
+        cout << "Your character = Sanji";
+        return 0;
+    }
+}
